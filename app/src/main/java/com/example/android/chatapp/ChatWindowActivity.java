a8128ea0c4a07c6e11fb0dd1a0f581e6d2c9607b
Azurion7 <43432117+Azurion7@users.noreply.github.com>
2018-10-17 22:45:35 +0530

    toolbar done

--- app/src/main/java/com/example/android/chatapp/ChatWindowActivity.java
+++ app/src/main/java/com/example/android/chatapp/ChatWindowActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.chatapp;
 
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,12 +22,22 @@ public class ChatWindowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_window);
 
-        userNameTextView=findViewById(R.id.chatWindowUserName);
-        displayPicImageView=findViewById(R.id.chatWindowDisplayPic);
+        Toolbar mToolbar = findViewById(R.id.userToolbar);
 
-        Intent intent=getIntent();
-        String userName=intent.getStringExtra("User Name");
-        String displayPicUrl=intent.getStringExtra("Image Url");
+        AppCompatActivity activity = ChatWindowActivity.this;
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final Drawable leftArrow =  ContextCompat.getDrawable(ChatWindowActivity.this, R.drawable.abc_ic_ab_back_material);
+        leftArrow.setColorFilter(ContextCompat.getColor(ChatWindowActivity.this, R.color.white), PorterDuff.Mode.SRC_ATOP);
+        activity.getSupportActionBar().setHomeAsUpIndicator(leftArrow);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        userNameTextView=findViewById(R.id.toolbarUsername);
+        displayPicImageView=findViewById(R.id.toolbarIcon);
+
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra("User Name");
+        String displayPicUrl = intent.getStringExtra("Image Url");
         userNameTextView.setText(userName);
         Glide.with(this).load(displayPicUrl).into(displayPicImageView);
 
