7f024f415f5e8856cbb63144ca809dd798e60b9e
Aman Agrawal <aman.agrawal.spit.ac.in>
2018-10-18 15:45:34 +0530

    Recent chats shows correctly

--- app/src/main/java/com/example/android/chatapp/ChatItem.java
+++ app/src/main/java/com/example/android/chatapp/ChatItem.java
@@ -3,17 +3,28 @@ package com.example.android.chatapp;
 public class ChatItem {
     private String imagePath;
     private String userName;
+    private String userKey;
+    private String chatRef;
 
-    public ChatItem(String imagePath, String userName) {
+
+
+    public ChatItem(String imagePath, String userName, String userKey, String chatRef) {
         this.imagePath = imagePath;
         this.userName = userName;
+        this.userKey=userKey;
+        this.chatRef=chatRef;
     }
-
+    public String getChatRef() {
+        return chatRef;
+    }
     public String getImagePath() {
         return imagePath;
     }
 
     public String getUserName() {
         return userName;
+    }
+    public String getUserKey() {
+        return userKey;
     }
 }
--- app/src/main/java/com/example/android/chatapp/Tab1_ChatsFragment.java
+++ app/src/main/java/com/example/android/chatapp/Tab1_ChatsFragment.java
@@ -6,12 +6,22 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class Tab1_ChatsFragment extends Fragment {
 
@@ -19,6 +29,8 @@ public class Tab1_ChatsFragment extends Fragment {
     RecyclerView recyclerView;
     ChatsAdapter chatsAdapter;
     List<ChatItem> chatItemList;
+    private ProgressBar progressBar;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -30,13 +42,40 @@ public class Tab1_ChatsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
         chatItemList = new ArrayList<>();
+        OverviewActivity.adapterFlag=-1;
         recyclerView = rootView.findViewById(R.id.chatsRecyclerView);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        //TODO
-        //add items to list
-        chatsAdapter = new ChatsAdapter(getActivity(), chatItemList);
-        recyclerView.setAdapter(chatsAdapter);
+        progressBar = rootView.findViewById(R.id.chatsProgressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        FirebaseDatabase.getInstance().getReference().child("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Chats").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot d: dataSnapshot.getChildren()){
+                    String userName=d.child("userName").getValue(String.class);
+                    Log.i(TAG, "onDataChange: "+userName);
+                    String displayPicUrl=d.child("imagePath").getValue(String.class);
+                    String userKey=d.child("userKey").getValue(String.class);
+                    String chatRef=d.getKey();
+                    chatItemList.add(new ChatItem(displayPicUrl,userName,userKey,chatRef));
+                }
+                chatsAdapter = new ChatsAdapter(getActivity(), chatItemList);
+                recyclerView.setAdapter(chatsAdapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
