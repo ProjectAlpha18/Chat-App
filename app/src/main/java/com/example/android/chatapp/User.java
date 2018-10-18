478b66be7c2c0bfe7ce923a9912f4965dec089b8
Tanvi Dhope <tanvidhope@gmail.com>
2018-10-18 14:14:42 +0530

    minor changes

--- app/src/main/java/com/example/android/chatapp/User.java
+++ app/src/main/java/com/example/android/chatapp/User.java
@@ -14,4 +14,13 @@ public class User {
         this.username = username;
         this.displayPictureLink = "https://firebasestorage.googleapis.com/v0/b/chatapp-5d2d8.appspot.com/o/deadmau5_facebook_dp_by_earcl01-d4lnn7f.jpg?alt=media&token=7e61a2d1-22fa-42dc-801a-438785eb3468";
     }
+
+    public void setUsername(String mUsername)
+    {
+        username = mUsername;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
